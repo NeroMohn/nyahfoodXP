75b36c618d9244e128a4f94d2916ec06c692aacb
David Rodrigues <davidrod.contato@gmail.com>
2019-05-01 16:37:51 -0300

    Correcao de query

--- src/java/dao/ComidaDAO.java
+++ src/java/dao/ComidaDAO.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 import model.Comida;
+import model.Loja;
 
 
 /**
@@ -90,7 +91,30 @@ public class ComidaDAO {
         List<Comida> comidas = null;
         try{
             tx.begin();
-            TypedQuery<Comida> query = em.createQuery("select co from Comida co", Comida.class);
+            TypedQuery<Comida> query = em.createQuery("select co from Comida co ", Comida.class);
+            comidas = query.getResultList();
+            tx.commit();
+        } catch (Exception e){
+            if(tx != null && tx.isActive()){
+                tx.rollback();
+            }
+            throw new RuntimeException(e);
+        }finally{
+            PersistenceUtil.close(em);
+        }
+        return comidas;
+    }
+    
+        public List<Comida> getAllComidasFromLoja(Long id){
+        
+        Loja loja= LojaDAO.getInstance().getLoja(id);
+        EntityManager em = PersistenceUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        List<Comida> comidas = null;
+        try{
+            tx.begin();
+            TypedQuery<Comida> query = em.createQuery("select co from Comida co where co.loja LIKE loja ", Comida.class);
+            query.setParameter("loja", loja);
             comidas = query.getResultList();
             tx.commit();
         } catch (Exception e){
